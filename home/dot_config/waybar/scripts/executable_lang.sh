f6c7a0f9c222567d9b70b6bf468e7c7ba8227e5b
Big-Ouden <simon.belier29@gmail.com>
2026-05-23 11:56:22 +0200

    update waybar

--- home/dot_config/waybar/scripts/executable_lang.sh
+++ home/dot_config/waybar/scripts/executable_lang.sh
@@ -3,8 +3,8 @@
 # Récupère la langue active depuis hyprctl
 lang=$(hyprctl -j devices | jq -r '.keyboards[] | select(.main) | .active_keymap' 2>/dev/null)
 
-if [[ "$lang" == "*French*" ]]; then
-    echo ""   # N'affiche rien (ou mets un texte si tu veux)
+if [[ "$lang" == *"French"* ]]; then
+    echo "FR"   # N'affiche rien (ou mets un texte si tu veux)
 fi
 if [[ "$lang" == "English (UK, Mac custom)" ]]; then
     echo "CST"
