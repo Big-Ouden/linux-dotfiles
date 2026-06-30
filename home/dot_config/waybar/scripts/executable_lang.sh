7ccc14446eed19d2c45ef674f476d9a70c9fd64d
Big-Ouden <simon.belier29@gmail.com>
2026-06-30 19:55:09 +0200

    add ergol

--- home/dot_config/waybar/scripts/executable_lang.sh
+++ home/dot_config/waybar/scripts/executable_lang.sh
@@ -3,11 +3,11 @@
 # Récupère la langue active depuis hyprctl
 lang=$(hyprctl -j devices | jq -r '.keyboards[] | select(.main) | .active_keymap' 2>/dev/null)
 
+if [[ "$lang" == *"French (Ergo-L)"* ]]; then
+    echo "ERGL"
+fi
 if [[ "$lang" == *"French"* ]]; then
     echo "FR"   # N'affiche rien (ou mets un texte si tu veux)
-fi
-if [[ "$lang" == *"ergol"* ]]; then
-    echo "ERGL"
 fi
 if [[ "$lang" == "English (UK, Mac custom)" ]]; then
     echo "MAC"
